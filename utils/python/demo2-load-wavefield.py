1c0b5b7842131497db8b43b3fafca23c90dd16d5
Dave May <dave.mayhem23@gmail.com>
2020-03-26 16:56:13 +0000

    JSON loader demo update: * Only import required modules * Provide example loading heavy data via JSON metafile * JSON reader always returns a dict * Provide example querying fields associated with wavefield

--- utils/python/demo2-load-wavefield.py
+++ utils/python/demo2-load-wavefield.py
@@ -6,15 +6,11 @@
 #   export PYTHONPATH=${SE2WAVE_ROOT}/utils/python:${PETSC_DIR}/lib/petsc/bin
 #
 
-import json as j
-import numpy as np
-import matplotlib.pyplot as plt
-
 import PetscBinaryIO as pio
 
 
 def se2wave_load_coordinates(filename):
-  debug = True
+  debug = False
   io = pio.PetscBinaryIO()
   data = dict()
   with open(filename) as fp:
@@ -35,15 +31,16 @@ def se2wave_load_coordinates(filename):
     data['coor'] = v
   
   if debug:
-    print('#elements-x',data['mx'])
-    print('#elements-y',data['my'])
-    print('#basis-x',data['nx'])
-    print('#basis-y',data['ny'])
+    print('<debug> se2wave_load_coordinates()')
+    print('<debug> #elements-x',data['mx'])
+    print('<debug> #elements-y',data['my'])
+    print('<debug> #basis-x',data['nx'])
+    print('<debug> #basis-y',data['ny'])
 
   return data
 
 def se2wave_load_wavefield(filename,has_displacement,has_velocity):
-  debug = True
+  debug = False
   io = pio.PetscBinaryIO()
   data = dict()
   with open(filename) as fp:
@@ -76,37 +73,73 @@ def se2wave_load_wavefield(filename,has_displacement,has_velocity):
       data['vel'] = v
 
   if debug:
-    print('#elements-x',data['mx'])
-    print('#elements-y',data['my'])
-    print('#basis-x',data['nx'])
-    print('#basis-y',data['ny'])
-    print('step',data['step'])
-    print('time',data['time'])
+    print('<debug> se2wave_load_wavefield()')
+    print('<debug> #elements-x',data['mx'])
+    print('<debug> #elements-y',data['my'])
+    print('<debug> #basis-x',data['nx'])
+    print('<debug> #basis-y',data['ny'])
+    print('<debug> step',data['step'])
+    print('<debug> time',data['time'])
   
   return data
 
 
-def se2wave_load_wavefield_from_jsonmeta(filename):
+def se2wave_load_json(filename):
+  import json as j
+  
+  debug = True
+  jdata = dict()
   with open(filename, "r") as fp:
     jdata = j.load(fp)
 
-
-    wdata = jdata['se2wave_wavefield']
-    print('nx:',wdata['nx'])
-    print('time:',wdata['time'])
-    print('fields:',wdata['fields'])
-    print('datafilename:',wdata['data']['filename'])
-
+    if debug:
+      wdata = jdata['se2wave']
+      print('<debug> se2wave_load_json()')
+      print('<debug> spatial_dimension:',wdata['spatial_dimension'])
+      print('<debug> nx:',wdata['nx'])
+      print('<debug> time:',wdata['time'])
+      print('<debug> fields:',wdata['data']['fields'])
+      print('<debug> datafilename:',wdata['data']['filename'])
+  return jdata
 
 
 path = "/Users/dmay/codes/se2wave-dev/se2wave/"
-filename = path + "default_mesh_coor.pbin"
-se2_coor = se2wave_load_coordinates(filename);
-print(se2_coor['coor'])
-
-w_filename = path + "step-1400_wavefield.pbin"
-se2_field = se2wave_load_wavefield(w_filename,True,True);
-print(se2_field['displ'])
 
 m_filename = path + "step-0100_wavefield.json"
-se2wave_load_wavefield_from_jsonmeta(m_filename)
+se2 = se2wave_load_json(m_filename)
+print('wavefield filename ->',se2['se2wave']['data']['filename'])
+has_displacement = False
+has_velocity = False
+fields = se2['se2wave']['data']['fields']
+if 'u' in fields:
+  has_displacement = True
+if 'v' in fields:
+  has_velocity = True
+print('fields:',se2['se2wave']['data']['fields'])
+print('fields: displacement?',has_displacement)
+print('fields: vel?',has_velocity)
+
+se2_field = se2wave_load_wavefield(se2['se2wave']['data']['filename'],has_displacement,has_velocity);
+
+if has_displacement:
+  mm = min(abs(se2_field['displ']))
+  print('min |u|',mm)
+  mm = max(abs(se2_field['displ']))
+  print('max |u|',mm)
+if has_velocity:
+  mm = min(abs(se2_field['vel']))
+  print('min |v|',mm)
+  mm = max(abs(se2_field['vel']))
+  print('max |v|',mm)
+
+m_filename = path + "default_mesh_coor.json"
+se2 = se2wave_load_json(m_filename)
+print('coor filename ->',se2['se2wave']['data']['filename'])
+
+se2_coor = se2wave_load_coordinates(se2['se2wave']['data']['filename']);
+print(se2_coor['coor'])
+
+
+
+
+
