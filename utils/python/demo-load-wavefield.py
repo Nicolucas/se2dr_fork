c5798b9df5feed8a253c9ac7771741d0df468f7b
Dave May <dave.mayhem23@gmail.com>
2020-03-31 07:54:20 +0100

    Modularised loaders * loader moved to separate file * Updated demos

--- utils/python/demo-load-wavefield.py
+++ utils/python/demo-load-wavefield.py
@@ -5,90 +5,14 @@
 # e.g.
 #   export PYTHONPATH=${SE2WAVE_ROOT}/utils/python:${PETSC_DIR}/lib/petsc/bin
 #
-import numpy as np
-import matplotlib.pyplot as plt
 
-import PetscBinaryIO as pio
-
-
-def se2wave_load_coordinates(filename):
-  debug = True
-  io = pio.PetscBinaryIO()
-  data = dict()
-  with open(filename) as fp:
-    v = io.readInteger(fp)
-    data['mx'] = v
-    
-    v = io.readInteger(fp)
-    data['my'] = v
-    
-    v = io.readInteger(fp)
-    data['nx'] = v
-    
-    v = io.readInteger(fp)
-    data['ny'] = v
-    
-    objecttype = io.readObjectType(fp)
-    v = io.readVec(fp)
-    data['coor'] = v
-  
-  if debug:
-    print('#elements-x',data['mx'])
-    print('#elements-y',data['my'])
-    print('#basis-x',data['nx'])
-    print('#basis-y',data['ny'])
-
-  return data
-
-def se2wave_load_wavefield(filename,has_displacement,has_velocity):
-  debug = True
-  io = pio.PetscBinaryIO()
-  data = dict()
-  with open(filename) as fp:
-    v = io.readInteger(fp)
-    data['mx'] = v
-    
-    v = io.readInteger(fp)
-    data['my'] = v
-    
-    v = io.readInteger(fp)
-    data['nx'] = v
-    
-    v = io.readInteger(fp)
-    data['ny'] = v
-
-    v = io.readInteger(fp)
-    data['step'] = v
-
-    v = io.readReal(fp)
-    data['time'] = v
-
-    if has_displacement:
-      objecttype = io.readObjectType(fp)
-      v = io.readVec(fp)
-      data['displ'] = v
-
-    if has_velocity:
-      objecttype = io.readObjectType(fp)
-      v = io.readVec(fp)
-      data['vel'] = v
-
-  if debug:
-    print('#elements-x',data['mx'])
-    print('#elements-y',data['my'])
-    print('#basis-x',data['nx'])
-    print('#basis-y',data['ny'])
-    print('step',data['step'])
-    print('time',data['time'])
-  
-  return data
-
+import se2waveload as load
 
 filename = "/Users/dmay/codes/se2wave-dev/se2wave/default_mesh_coor.pbin"
-se2_coor = se2wave_load_coordinates(filename);
+se2_coor = load.se2wave_load_coordinates(filename);
 print(se2_coor['coor'])
 
 w_filename = "/Users/dmay/codes/se2wave-dev/se2wave/step-1400_wavefield.pbin"
-se2_field = se2wave_load_wavefield(w_filename,True,True);
+se2_field = load.se2wave_load_wavefield(w_filename,True,True);
 print(se2_field['displ'])
 
--- utils/python/demo2-load-wavefield.py
+++ utils/python/demo2-load-wavefield.py
@@ -6,107 +6,12 @@
 #   export PYTHONPATH=${SE2WAVE_ROOT}/utils/python:${PETSC_DIR}/lib/petsc/bin
 #
 
-import PetscBinaryIO as pio
-
-
-def se2wave_load_coordinates(filename):
-  debug = False
-  io = pio.PetscBinaryIO()
-  data = dict()
-  with open(filename) as fp:
-    v = io.readInteger(fp)
-    data['mx'] = v
-    
-    v = io.readInteger(fp)
-    data['my'] = v
-    
-    v = io.readInteger(fp)
-    data['nx'] = v
-    
-    v = io.readInteger(fp)
-    data['ny'] = v
-    
-    objecttype = io.readObjectType(fp)
-    v = io.readVec(fp)
-    data['coor'] = v
-  
-  if debug:
-    print('<debug> se2wave_load_coordinates()')
-    print('<debug> #elements-x',data['mx'])
-    print('<debug> #elements-y',data['my'])
-    print('<debug> #basis-x',data['nx'])
-    print('<debug> #basis-y',data['ny'])
-
-  return data
-
-def se2wave_load_wavefield(filename,has_displacement,has_velocity):
-  debug = False
-  io = pio.PetscBinaryIO()
-  data = dict()
-  with open(filename) as fp:
-    v = io.readInteger(fp)
-    data['mx'] = v
-    
-    v = io.readInteger(fp)
-    data['my'] = v
-    
-    v = io.readInteger(fp)
-    data['nx'] = v
-    
-    v = io.readInteger(fp)
-    data['ny'] = v
-
-    v = io.readInteger(fp)
-    data['step'] = v
-
-    v = io.readReal(fp)
-    data['time'] = v
-
-    if has_displacement:
-      objecttype = io.readObjectType(fp)
-      v = io.readVec(fp)
-      data['displ'] = v
-
-    if has_velocity:
-      objecttype = io.readObjectType(fp)
-      v = io.readVec(fp)
-      data['vel'] = v
-
-  if debug:
-    print('<debug> se2wave_load_wavefield()')
-    print('<debug> #elements-x',data['mx'])
-    print('<debug> #elements-y',data['my'])
-    print('<debug> #basis-x',data['nx'])
-    print('<debug> #basis-y',data['ny'])
-    print('<debug> step',data['step'])
-    print('<debug> time',data['time'])
-  
-  return data
-
-
-def se2wave_load_json(filename):
-  import json as j
-  
-  debug = True
-  jdata = dict()
-  with open(filename, "r") as fp:
-    jdata = j.load(fp)
-
-    if debug:
-      wdata = jdata['se2wave']
-      print('<debug> se2wave_load_json()')
-      print('<debug> spatial_dimension:',wdata['spatial_dimension'])
-      print('<debug> nx:',wdata['nx'])
-      print('<debug> time:',wdata['time'])
-      print('<debug> fields:',wdata['data']['fields'])
-      print('<debug> datafilename:',wdata['data']['filename'])
-  return jdata
-
+import se2waveload as load
 
 path = "/Users/dmay/codes/se2wave-dev/se2wave/"
 
 m_filename = path + "step-0100_wavefield.json"
-se2 = se2wave_load_json(m_filename)
+se2 = load.se2wave_load_json(m_filename,debug=True)
 print('wavefield filename ->',se2['se2wave']['data']['filename'])
 has_displacement = False
 has_velocity = False
@@ -119,7 +24,8 @@ print('fields:',se2['se2wave']['data']['fields'])
 print('fields: displacement?',has_displacement)
 print('fields: vel?',has_velocity)
 
-se2_field = se2wave_load_wavefield(se2['se2wave']['data']['filename'],has_displacement,has_velocity);
+heavydata_filename = path + se2['se2wave']['data']['filename']
+se2_field = load.se2wave_load_wavefield(heavydata_filename,has_displacement,has_velocity,debug=True);
 
 if has_displacement:
   mm = min(abs(se2_field['displ']))
@@ -133,10 +39,11 @@ if has_velocity:
   print('max |v|',mm)
 
 m_filename = path + "default_mesh_coor.json"
-se2 = se2wave_load_json(m_filename)
+se2 = load.se2wave_load_json(m_filename,debug=True)
 print('coor filename ->',se2['se2wave']['data']['filename'])
 
-se2_coor = se2wave_load_coordinates(se2['se2wave']['data']['filename']);
+heavydata_filename = path + se2['se2wave']['data']['filename']
+se2_coor = load.se2wave_load_coordinates(heavydata_filename,debug=True);
 print(se2_coor['coor'])
 
 
